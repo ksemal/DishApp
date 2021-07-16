4cf6beee705da61a184ca7c3ebe3a2dcfdf49a6e
Ksenia Malyshev <starkcenia@gmail.com>
2021-07-16 11:25:16 -0400

    fix: added click listener

--- app/src/main/java/com/example/dishapp/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/example/dishapp/view/activities/AddUpdateDishActivity.kt
@@ -1,12 +1,36 @@
 package com.example.dishapp.view.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.dishapp.R
+import com.example.dishapp.databinding.ActivityAddUpdateDishBinding
 
-class AddUpdateDishActivity : AppCompatActivity() {
+class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var mBinding: ActivityAddUpdateDishBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dish)
+        mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        setUpActionBar()
+        mBinding.ivAddDishImage.setOnClickListener(this)
+    }
+
+    private fun setUpActionBar() {
+        setSupportActionBar(mBinding.updateAddDishToolbar)
+        supportActionBar?.setHomeButtonEnabled(true)
+        mBinding.updateAddDishToolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.iv_add_dish_image -> {
+                Toast.makeText(this, "You have clicked the image", Toast.LENGTH_SHORT).show()
+                return
+            }
+        }
     }
 }
