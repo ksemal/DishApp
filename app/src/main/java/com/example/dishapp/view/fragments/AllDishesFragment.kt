833613c4dcdc6fe00a6b8cca78c567666525d7de
Ksenia Malyshev <starkcenia@gmail.com>
2021-07-16 08:46:03 -0400

    added new activity

---
+++ app/src/main/java/com/example/dishapp/view/fragments/AllDishesFragment.kt
@@ -0,0 +1,52 @@
+package com.example.dishapp.view.fragments
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.*
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.dishapp.R
+import com.example.dishapp.view.activities.AddUpdateDishActivity
+import com.example.dishapp.viewmodel.HomeViewModel
+
+class AllDishesFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_all_dishes, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+       return  when(item.itemId) {
+            R.id.action_add_dish -> {
+                startActivity(Intent(requireActivity(), AddUpdateDishActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_all_dishes, container, false)
+        val textView: TextView = root.findViewById(R.id.text_home)
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
