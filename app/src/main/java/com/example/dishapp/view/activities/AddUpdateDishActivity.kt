4c7e002f57523fd2e8e72a64bfe8d56b2cc3620c
Ksenia Malyshev <starkcenia@gmail.com>
2021-07-16 11:43:33 -0400

    feat: dialog

--- app/src/main/java/com/example/dishapp/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/example/dishapp/view/activities/AddUpdateDishActivity.kt
@@ -1,11 +1,16 @@
 package com.example.dishapp.view.activities
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
 import com.example.dishapp.R
 import com.example.dishapp.databinding.ActivityAddUpdateDishBinding
+import com.example.dishapp.databinding.DialogCustomImageSelectionBinding
 
 class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var mBinding: ActivityAddUpdateDishBinding
@@ -28,9 +33,25 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.iv_add_dish_image -> {
-                Toast.makeText(this, "You have clicked the image", Toast.LENGTH_SHORT).show()
+                customImageSelectionDialog()
                 return
             }
         }
+    }
+
+    private fun customImageSelectionDialog() {
+        val dialog = Dialog(this)
+        val binding: DialogCustomImageSelectionBinding = DialogCustomImageSelectionBinding.inflate(layoutInflater)
+        dialog.setContentView(binding.root)
+        dialog.window?.setBackgroundDrawable(AppCompatResources.getDrawable(this, R.drawable.dialog_background))
+        binding.tvCamera.setOnClickListener {
+            Toast.makeText(this, "Camera clicked", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+        binding.tvGallery.setOnClickListener {
+            Toast.makeText(this, "Gallery clicked", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+        dialog.show()
     }
 }
