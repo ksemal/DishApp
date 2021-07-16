cdd135f631c49aeb7b00a7936c63c0bb432c514d
Ksenia Malyshev <starkcenia@gmail.com>
2021-07-16 12:18:02 -0400

    feat: added permissions

--- app/src/main/java/com/example/dishapp/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/example/dishapp/view/activities/AddUpdateDishActivity.kt
@@ -1,9 +1,13 @@
 package com.example.dishapp.view.activities
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.Dialog
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +15,11 @@ import androidx.appcompat.content.res.AppCompatResources
 import com.example.dishapp.R
 import com.example.dishapp.databinding.ActivityAddUpdateDishBinding
 import com.example.dishapp.databinding.DialogCustomImageSelectionBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
 class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var mBinding: ActivityAddUpdateDishBinding
@@ -41,17 +50,97 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun customImageSelectionDialog() {
         val dialog = Dialog(this)
-        val binding: DialogCustomImageSelectionBinding = DialogCustomImageSelectionBinding.inflate(layoutInflater)
+        val binding: DialogCustomImageSelectionBinding =
+            DialogCustomImageSelectionBinding.inflate(layoutInflater)
         dialog.setContentView(binding.root)
-        dialog.window?.setBackgroundDrawable(AppCompatResources.getDrawable(this, R.drawable.dialog_background))
+        dialog.window?.setBackgroundDrawable(
+            AppCompatResources.getDrawable(
+                this,
+                R.drawable.dialog_background
+            )
+        )
         binding.tvCamera.setOnClickListener {
-            Toast.makeText(this, "Camera clicked", Toast.LENGTH_SHORT).show()
+            Dexter.withContext(this)
+                .withPermissions(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.CAMERA
+                )
+                .withListener(object : MultiplePermissionsListener {
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                        if (report != null && report.areAllPermissionsGranted()) {
+                            Toast.makeText(
+                                this@AddUpdateDishActivity,
+                                "You have camera permission",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else {
+                            showRationalDialogForPermissions()
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permissions: MutableList<PermissionRequest>?,
+                        token: PermissionToken?
+                    ) {
+                        showRationalDialogForPermissions()
+                    }
+
+                })
+                .onSameThread()
+                .check()
             dialog.dismiss()
         }
         binding.tvGallery.setOnClickListener {
-            Toast.makeText(this, "Gallery clicked", Toast.LENGTH_SHORT).show()
+            Dexter.withContext(this)
+                .withPermissions(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+                .withListener(object : MultiplePermissionsListener {
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                        if (report != null && report.areAllPermissionsGranted()) {
+                            Toast.makeText(
+                                this@AddUpdateDishActivity,
+                                "You have gallery permission",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else {
+                            showRationalDialogForPermissions()
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permissions: MutableList<PermissionRequest>?,
+                        token: PermissionToken?
+                    ) {
+                        showRationalDialogForPermissions()
+                    }
+
+                })
+                .onSameThread()
+                .check()
             dialog.dismiss()
         }
         dialog.show()
+    }
+
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage(R.string.add_image_dialog_no_permission)
+            .setPositiveButton(R.string.add_image_dialog_positive_btn) { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton(R.string.add_image_dialog_negative_btn) { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
     }
 }
