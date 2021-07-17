fade5f339a1ce384c7d9d06a8e1dce74312f0983
Ksenia Malyshev <starkcenia@gmail.com>
2021-07-16 22:48:10 -0400

    feat: saved image pathes

--- app/src/main/java/com/example/dishapp/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/example/dishapp/view/activities/AddUpdateDishActivity.kt
@@ -5,8 +5,11 @@ import android.app.Activity
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.ContextWrapper
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
@@ -17,7 +20,13 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.graphics.drawable.toBitmap
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.dishapp.DATA_KEY
 import com.example.dishapp.R
 import com.example.dishapp.databinding.ActivityAddUpdateDishBinding
@@ -30,9 +39,16 @@ import com.karumi.dexter.listener.PermissionGrantedResponse
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import com.karumi.dexter.listener.single.PermissionListener
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.util.*
 
 class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var mBinding: ActivityAddUpdateDishBinding
+    private var mImagePath: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
@@ -165,6 +181,9 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
                             .load(thumbnail)
                             .centerCrop()
                             .into(mBinding.ivDishImage)
+
+                        mImagePath = saveImageToInternalStorage(thumbnail)
+                        Log.i("imagePath", mImagePath)
                         mBinding.ivAddDishImage.setImageDrawable(
                             AppCompatResources.getDrawable(
                                 this@AddUpdateDishActivity,
@@ -188,6 +207,34 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
                         Glide.with(this)
                             .load(selectedPhotoUri)
                             .centerCrop()
+                            .diskCacheStrategy(DiskCacheStrategy.ALL)
+                            .listener(object : RequestListener<Drawable> {
+                                override fun onLoadFailed(
+                                    e: GlideException?,
+                                    model: Any?,
+                                    target: Target<Drawable>?,
+                                    isFirstResource: Boolean
+                                ): Boolean {
+                                    Log.e("TAG", "Error loading image", e)
+                                    return false
+                                }
+
+                                override fun onResourceReady(
+                                    resource: Drawable?,
+                                    model: Any?,
+                                    target: Target<Drawable>?,
+                                    dataSource: DataSource?,
+                                    isFirstResource: Boolean
+                                ): Boolean {
+                                    resource?.let { drawable ->
+                                        val bitmap = drawable.toBitmap()
+                                        mImagePath = saveImageToInternalStorage(bitmap)
+                                        Log.i("imagePath", mImagePath)
+                                    }
+                                    return false
+                                }
+
+                            })
                             .into(mBinding.ivDishImage)
                         mBinding.ivAddDishImage.setImageDrawable(
                             AppCompatResources.getDrawable(
@@ -203,4 +250,24 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
                 }
             }
         }
+
+    private fun saveImageToInternalStorage(bitmap: Bitmap?): String {
+        val wrapper = ContextWrapper(applicationContext)
+        val path = wrapper.getDir(IMAGE_DIRECTORY, Context.MODE_PRIVATE)
+        val file = File(path, "${UUID.randomUUID()}.jpg")
+
+        try {
+            val stream: OutputStream = FileOutputStream(file)
+            bitmap?.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+            stream.flush()
+            stream.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return file.absolutePath
+    }
+
+    companion object {
+        const val IMAGE_DIRECTORY = "DishAppImages"
+    }
 }
