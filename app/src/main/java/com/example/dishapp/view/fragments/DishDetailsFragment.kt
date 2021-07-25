267898401f851076015e9a613f9dc4321224601c
Ksenia Malyshev <starkcenia@gmail.com>
2021-07-24 23:04:54 -0400

    feat: added dish details layout with data

--- app/src/main/java/com/example/dishapp/view/fragments/DishDetailsFragment.kt
+++ app/src/main/java/com/example/dishapp/view/fragments/DishDetailsFragment.kt
@@ -1,26 +1,55 @@
 package com.example.dishapp.view.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.bumptech.glide.Glide
 import com.example.dishapp.R
+import com.example.dishapp.databinding.FragmentDishDetailsBinding
 import com.example.dishapp.viewmodel.DishViewModel
+import java.util.*
 
 class DishDetailsFragment : Fragment() {
 
     private val viewModel: DishViewModel by activityViewModels()
+    private var mBinding: FragmentDishDetailsBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-       val root = inflater.inflate(R.layout.fragment_dish_details, container, false)
-        println("Selected dish ${viewModel.getSelectedDish()?.title}")
-        return root
+        mBinding = FragmentDishDetailsBinding.inflate(inflater, container, false)
+        return mBinding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.getSelectedDish()?.let { dish ->
+            mBinding?.let { binding ->
+                Glide.with(requireActivity())
+                    .load(dish.image)
+                    .centerCrop()
+                    .into(binding.ivDishImage)
+
+                binding.tvTitle.text = dish.title
+                binding.tvType.text =
+                    dish.type.capitalize(Locale.ROOT)
+                binding.tvCategory.text = dish.category
+                binding.tvIngredients.text = dish.ingredients
+                binding.tvCookingDirection.text = dish.directionsToCook
+                binding.tvCookingTime.text =
+                    resources.getString(R.string.lbl_estimate_cooking_time, dish.cookingTime)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mBinding = null
     }
 
 }
