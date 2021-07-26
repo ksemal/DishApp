e3e2ff0156a35def64a566ce427a96734932c592
Ksenia Malyshev <starkcenia@gmail.com>
2021-07-26 11:37:38 -0400

    feat: added palette

--- app/src/main/java/com/example/dishapp/view/fragments/DishDetailsFragment.kt
+++ app/src/main/java/com/example/dishapp/view/fragments/DishDetailsFragment.kt
@@ -1,12 +1,20 @@
 package com.example.dishapp.view.fragments
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.palette.graphics.Palette
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.dishapp.R
 import com.example.dishapp.databinding.FragmentDishDetailsBinding
 import com.example.dishapp.viewmodel.DishViewModel
@@ -33,6 +41,33 @@ class DishDetailsFragment : Fragment() {
                 Glide.with(requireActivity())
                     .load(dish.image)
                     .centerCrop()
+                    .listener(object : RequestListener<Drawable> {
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            Log.e("TAG", "Error loading image", e)
+                            return false
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            dataSource: DataSource?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            resource?.let {
+                                Palette.from(it.toBitmap()).generate { palette ->
+                                    val intColor = palette?.vibrantSwatch?.rgb ?: 0
+                                    mBinding?.rlDishDetailMain?.setBackgroundColor(intColor)
+                                }
+                            }
+                            return false
+                        }
+                    })
                     .into(binding.ivDishImage)
 
                 binding.tvTitle.text = dish.title
